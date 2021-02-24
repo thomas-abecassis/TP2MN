ff0807a22a2dc38af673c9adfd7e807b8fbd6bd2
Thomas Abecassis <stingerkiller34@gmail.com>
2021-02-24 14:12:37 +0100

    multiplication

--- src/complexe.c
+++ src/complexe.c
@@ -24,12 +24,8 @@ complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_
 {
   complexe_float_t r ;
 
-  /* 
-     a implementer
-  */
-
-  r.real = 0.0 ;
-  r.imaginary = 0.0 ;
+  r.real = c1.real * c2.real + c1.imaginary*c2.imaginary;
+  r.imaginary = 2 * c1.real * c2.imaginary;
   
   return r ;
 }
@@ -38,12 +34,8 @@ complexe_double_t mult_complexe_double (const complexe_double_t c1, const comple
   {
   complexe_double_t r ;
 
-  /* 
-     a implementer
-  */
-  
-  r.real = 0.0 ;
-  r.imaginary = 0.0 ;
+  r.real = c1.real * c2.real + c1.imaginary*c2.imaginary;
+  r.imaginary = 2 * c1.real * c2.imaginary;
   
   return r ;
 }

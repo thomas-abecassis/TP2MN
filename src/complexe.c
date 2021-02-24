7802953208014b9e45d3db37398f837f91d0c4e5
Thomas Abecassis <stingerkiller34@gmail.com>
2021-02-24 15:34:53 +0100

    correction 2

--- src/complexe.c
+++ src/complexe.c
@@ -57,8 +57,8 @@ complexe_double_t add_complexe_double (const complexe_double_t c1, const complex
 complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
 {
   complexe_float_t r ;
-  r.real = c1.real * c2.real + c1.imaginary*c2.imaginary;
-  r.imaginary = 2 * c1.real * c2.imaginary;
+  r.real = c1.real * c2.real - c1.imaginary*c2.imaginary;
+  r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
   
   return r ;
 }
@@ -67,8 +67,8 @@ complexe_double_t mult_complexe_double (const complexe_double_t c1, const comple
   {
   complexe_double_t r ;
 
-  r.real = c1.real * c2.real + c1.imaginary*c2.imaginary;
-  r.imaginary = 2 * c1.real * c2.imaginary;
+  r.real = c1.real * c2.real - c1.imaginary*c2.imaginary;
+  r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
   
   return r ;
 }

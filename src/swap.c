f60f9c47cc1c8a2eee48c679ffd7756c0662f7e3
Thomas Abecassis <stingerkiller34@gmail.com>
2021-03-08 11:01:38 +0100

    swap

--- src/swap.c
+++ src/swap.c
@@ -1,4 +1,5 @@
 #include "mnblas.h"
+#include "complexe.h"
 
 void mncblas_sswap(const int N, float *X, const int incX, 
                  float *Y, const int incY)
@@ -20,21 +21,51 @@ void mncblas_sswap(const int N, float *X, const int incX,
 void mncblas_dswap(const int N, double *X, const int incX, 
                  double *Y, const int incY)
 {
+  register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
+  register double save ;
+  
+  for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
+    {
+      save = Y [j] ;
+      Y [j] = X [i] ;
+      X [i] = save ;
+    }
 
-  return ;
 }
 
 void mncblas_cswap(const int N, void *X, const int incX, 
 		                    void *Y, const int incY)
 {
+  register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
+  complexe_float_t* save;
+  complexe_float_t** Xc = X;
+  complexe_float_t** Yc = Y;
+  
+  for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
+    {
+      save = Yc [j] ;
+      Yc [j] = Xc [i] ;
+      Xc [i] = save ;
+    }
 
-  return ;
 }
 
 void mncblas_zswap(const int N, void *X, const int incX, 
 		                    void *Y, const int incY)
 {
-
-  return ;
+  register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
+  complexe_double_t* save;
+  complexe_double_t** Xc = X;
+  complexe_double_t** Yc = Y;
+  
+  for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
+    {
+      save = Yc [j] ;
+      Yc [j] = Xc [i] ;
+      Xc [i] = save ;
+    }
 }
 
